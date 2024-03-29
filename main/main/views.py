d6e8cd536fbb841ce6f5ed0436a6b1869ae77823
jlzz02 <jacopo.lazzaro.jl@gamil.com>
2024-03-29 19:27:28 +0100

    initial commit

---
+++ main/main/views.py
@@ -0,0 +1,47 @@
+from django.http import request
+from django.shortcuts import render
+from dotenv import load_dotenv
+import os
+import requests
+import pprint
+
+def get_weather(city, key):
+        request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={key}&q={city}&units=metric&lang=it'
+        weather_data = requests.get(request_url).json()
+        return weather_data
+
+def index(request):
+    city_info = ""
+    start_cities = ["torino", "roma, it", "firenze", "napoli", "palermo"]
+    load_dotenv()
+    key = os.getenv("API_KEY")
+
+    if request.session.get("start_cities_info"):
+         start_cities_info = request.session.get("start_cities_info")
+    else:
+        start_cities_info = []
+        for city in start_cities:
+                start_cities_info.append(get_weather(city, key))
+        
+        request.session["start_cities_info"] = start_cities_info
+
+
+
+
+    pprint.pprint(start_cities_info)
+         
+
+    if request.method == "POST":
+        
+        city = request.POST.get('city')
+          
+        if city:
+          city_info = get_weather(city, key)
+
+
+    return render(request, "index.html", {"city_info" : city_info, "start_cities_info" : start_cities_info})
+
+
+def city_page(request):
+     name = request.GET.get("id")
+     return render(request, "city.html", {"name" : name})
