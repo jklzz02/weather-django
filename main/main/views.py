93ed9e9db796b4da9c859ea7ad4681e9ea4de5e1
jlzz02 <jacopo.lazzaro.jl@gamil.com>
2024-03-30 00:00:46 +0100

    migliorati view e template city

--- main/main/views.py
+++ main/main/views.py
@@ -5,6 +5,9 @@ import os
 import requests
 import pprint
 
+load_dotenv()
+key = os.getenv("API_KEY")
+
 def get_weather(city, key):
         request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={key}&q={city}&units=metric&lang=it'
         weather_data = requests.get(request_url).json()
@@ -13,8 +16,6 @@ def get_weather(city, key):
 def index(request):
     city_info = ""
     start_cities = ["torino", "roma, it", "firenze", "napoli", "palermo"]
-    load_dotenv()
-    key = os.getenv("API_KEY")
 
     if request.session.get("start_cities_info"):
          start_cities_info = request.session.get("start_cities_info")
@@ -25,23 +26,25 @@ def index(request):
         
         request.session["start_cities_info"] = start_cities_info
 
-
-
-
-    pprint.pprint(start_cities_info)
-         
-
-    if request.method == "POST":
-        
-        city = request.POST.get('city')
-          
-        if city:
-          city_info = get_weather(city, key)
+    pprint.pprint(start_cities_info)     
 
 
     return render(request, "index.html", {"city_info" : city_info, "start_cities_info" : start_cities_info})
 
 
 def city_page(request):
-     name = request.GET.get("id").replace("/", "")
-     return render(request, "city.html", {"name" : name})
+     city = request.GET.get("id")
+
+     if city:
+          city = city.replace("/", "")
+     city_info = ""
+     if request.method == "POST":
+
+        city = request.POST.get('city')
+            
+     if city:
+        city_info = get_weather(city, key)
+       
+
+
+     return render(request, "city.html", {"city" : city, "city_info" : city_info})
