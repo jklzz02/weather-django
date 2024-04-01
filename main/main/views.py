4524aeb0a9d24a954a128c531c47ea91385cc9ca
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-04-01 10:29:46 +0200

    agiunta cache

--- main/main/views.py
+++ main/main/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from dotenv import load_dotenv
+from functools import lru_cache
 import requests
 import pprint
 import os
@@ -10,6 +11,8 @@ import os
 load_dotenv()
 key = os.getenv("API_KEY")
 map_key = os.getenv("MAP_KEY")
+
+@lru_cache
 def get_weather(city, key):
         request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={key}&q={city}&units=metric&lang=it'
         weather_data = requests.get(request_url).json()
@@ -24,13 +27,12 @@ def index(request):
          start_cities_info = request.session.get("start_cities_info")
          print("non lo sto facendo")
     else:
-        start_cities_info = []
-        for city in start_cities:
-                start_cities_info.append(get_weather(city, key))
-                print("lo sto facendo")
+     start_cities_info = []
+     for city in start_cities:
+                    start_cities_info.append(get_weather(city, key))
         
-        request.session["start_cities_info"] = start_cities_info
-
+    request.session["start_cities_info"] = start_cities_info
+    print(get_weather.cache_info())
 
     return render(request, "index.html", {"city_info" : city_info, "start_cities_info" : start_cities_info})
 
@@ -55,6 +57,7 @@ def city_page(request):
 
 
      pprint.pprint(city_info)    
+     print(get_weather.cache_info())
 
 
-     return render(request, "city.html", {"city" : city, "query" : query, "error" : error, "city_info" : city_info, "map_key" : map_key})
+     return render(request, "city.html", {"city" : city, "error" : error, "city_info" : city_info, "map_key" : map_key})
