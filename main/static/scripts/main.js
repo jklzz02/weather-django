678d5199c8c72401292335546021f0d0b5e133f1
jlzz02 <jacopo.lazzaro.jl@gamil.com>
2024-04-16 19:20:17 +0200

    creato toggle in javascript per light mode  aggiunti commenti al css

--- main/static/scripts/main.js
+++ main/static/scripts/main.js
@@ -1,1 +1,17 @@
-console.log("loaded")
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const theme = localStorage.getItem('theme');
+
+
+if (theme) {
+    document.body.classList.add(theme);
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+
+    if (document.body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light-mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
+});
