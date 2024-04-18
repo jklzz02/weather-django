ad29b4d64aeadb7d34ba576222072bcc9a62682f
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-04-18 02:24:23 +0200

    migliorato jscript e stile

--- main/static/scripts/main.js
+++ main/static/scripts/main.js
@@ -1,15 +1,12 @@
-// funzioni
-function goBack() {
-    window.history.back();
-}
-// elementi presi dall'html
+// html elements
 const themeToggleBtn = document.querySelector('.theme-toggle');
 const lightIcon = document.getElementById('lightIcon');
 const darkIcon = document.getElementById('darkIcon');
-// tema dal local storage
+
+// get current theme
 const theme = localStorage.getItem('theme');
 
-// logica appena attivato lo script
+// adding classes to the body and icons based on current theme
 if (theme) {
     document.body.classList.add(theme);
     darkIcon.classList.add('hidden');
@@ -18,7 +15,7 @@ else {
     lightIcon.classList.add('hidden');
 }
 
-// azione del bottone
+// toggle between icons and add to local storace current theme mode
 themeToggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
 
@@ -26,9 +23,19 @@ themeToggleBtn.addEventListener('click', () => {
         localStorage.setItem('theme', 'light-mode');
         darkIcon.classList.add('hidden');
         lightIcon.classList.remove('hidden');
+        lightIcon.classList.add('fade-in');
+
     } else {
         localStorage.removeItem('theme');
         lightIcon.classList.add('hidden');
         darkIcon.classList.remove('hidden');
+        darkIcon.classList.add('fade-in');
     }
+
+    // timeout that removes animation after it's finished
+    setTimeout(() => {
+        lightIcon.classList.remove('fade-in');
+        darkIcon.classList.remove('fade-in');
+    }, 500);
+
 });
