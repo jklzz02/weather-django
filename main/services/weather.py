bf33ddb33beb22483c5035c050109dedf18f124e
jlzz02 <jacopo.lazzaro.jl@gamil.com>
2024-10-13 15:27:36 +0200

    refactor

---
+++ main/services/weather.py
@@ -0,0 +1,79 @@
+from functools import lru_cache
+from requests import get, post, RequestException
+from django.conf import settings
+
+lang = settings.LANGUAGE_CODE
+
+__weather_key = settings.KEYS['weather_key']
+__air_key = settings.KEYS['air_key']
+__open_weather = 'https://api.openweathermap.org/'
+__google_aqi = 'https://airquality.googleapis.com/'
+
+@lru_cache
+def get_current_weather(city:str, lang:str=lang) -> dict|bool:
+    url = f'{__open_weather}/data/2.5/weather'
+    params = {
+            'appid' : __weather_key,
+            'q' : city,
+            'units' : 'metric',
+            'lang' : lang
+    }
+
+    current_weather = get_request(url, params)
+    return current_weather
+
+
+@lru_cache
+def get_forecast_weather(lat:str, lon:str, lang:str=lang) -> dict|bool:
+    url = f'{__open_weather}data/3.0/onecall'
+    params = {
+            'lat' : lat,
+            'lon' : lon,
+            'appid' : __weather_key,
+            'lang' : lang
+    }
+       
+    forecast_data = get_request(url, params)
+    return forecast_data
+
+@lru_cache
+def get_air_conditions(lat:str, lon:str, lang:str=lang) -> dict|bool:
+
+    url = f'{__google_aqi}v1/currentConditions:lookup'
+    params = {"key" : __air_key}
+    json = {
+        "location":
+          {
+            "latitude": lat,
+           "longitude": lon
+           },
+            "extraComputations": [
+                "DOMINANT_POLLUTANT_CONCENTRATION",
+                "POLLUTANT_CONCENTRATION",
+                "LOCAL_AQI",
+            ],
+            "languageCode": lang,
+        }
+        
+    air_conditions = post_request(url, params, json)
+    return air_conditions
+
+
+def get_request(url:str, params:dict) -> dict|bool:
+    try:
+        response = get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+    except RequestException as e:
+        code = e.response.status_code if e.response.status_code else "Unkown"
+        print(f'GET request failed with status code {code}')
+
+def post_request(url:str, params:dict, json:dict) -> dict|bool:
+    try:
+        response = post(url, params=params, json=json)
+        response.raise_for_status()
+        return response.json()
+    except RequestException as e:
+        code = e.response.status_code if e.response.status_code else "Unkown"
+        print(f'POST request failed with status code {code}')
+    
