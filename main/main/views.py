d477aa2c0286700862aeb59b1276934b208391fe
jlzz02 <jacopo.lazzaro.jl@gamil.com>
2024-10-20 17:43:01 +0200

    small refactor improved logic control of city view

--- main/main/views.py
+++ main/main/views.py
@@ -57,69 +57,63 @@ def city(request):
      city_info, forecast_weather, raw_air_conditions = asyncio.run(weather_info())
 
 
-     if not city_info:
-          error = True
-
-     if not error:
-
-          if not forecast_weather:
-               return render(request, "city.html", {"city" : city, "error" : True})
-
+     if not city_info or not forecast_weather:
+           return render(request, "city.html", {"city" : city, "error" : True})
           
-          if raw_air_conditions:
-               air_conditions["aqi"] = raw_air_conditions["indexes"][0]["aqiDisplay"]
+     if raw_air_conditions:
+          air_conditions["aqi"] = raw_air_conditions["indexes"][0]["aqiDisplay"]
 
-               if int(air_conditions["aqi"]) < 50:
-                    color = "green";
-               elif int(air_conditions["aqi"]) < 100:
-                    color = "yellow"
-               else:
-                    color = "red"
+          if int(air_conditions["aqi"]) < 50:
+               color = "green";
+          elif int(air_conditions["aqi"]) < 100:
+               color = "yellow"
+          else:
+               color = "red"
 
-               air_conditions["aqi_color"] = color
-               air_conditions["category"] = raw_air_conditions["indexes"][0]["category"]
-               air_conditions["dominant_pollutant"] = raw_air_conditions["indexes"][0]["dominantPollutant"]
-               air_conditions["pollutants"] = raw_air_conditions["pollutants"]
+          air_conditions["aqi_color"] = color
+          air_conditions["category"] = raw_air_conditions["indexes"][0]["category"]
+          air_conditions["dominant_pollutant"] = raw_air_conditions["indexes"][0]["dominantPollutant"]
+          air_conditions["pollutants"] = raw_air_conditions["pollutants"]
 
 
-          for day in forecast_weather["daily"]:
+     for day in forecast_weather["daily"]:
 
-               forecast_date = translate(unix_timestamp_converter(day['dt'], date_format="%A %d/%m/%Y"))
-               summary = translate(day["summary"])
-               weather = day["weather"]
-               temp = day["temp"]
-               humidity = day["humidity"]
-               wind = day["wind_speed"]
+          forecast_date = translate(unix_timestamp_converter(day['dt'], date_format="%A %d/%m/%Y"))
+          summary = translate(day["summary"])
+          weather = day["weather"]
+          temp = day["temp"]
+          humidity = day["humidity"]
+          wind = day["wind_speed"]
 
-               city_forecast = {
-                    "forecast_date" : forecast_date,
-                    "summary" : summary,
-                    "weather" : weather,
-                    "temp" : temp,
-                    "humidity" : humidity,
-                    "wind" : wind}
-               
-               forecast_info.append(city_forecast)
-
-          for i, hour in enumerate(forecast_weather["hourly"]):
-
-               if i >= 12:
-                    break
-
-               hour_date = unix_timestamp_converter(hour["dt"], date_format="%H:%M")
-               description = hour["weather"][0]["description"]
-               icon = hour["weather"][0]["icon"]
-               hour_temp = hour["temp"]
-
-               hour_city_forecast = {"hour" : hour_date, "description" : description, "icon" : icon, "temp" : hour_temp }
-               hourly_forecast.append(hour_city_forecast)
+          city_forecast = {
+               "forecast_date" : forecast_date,
+               "summary" : summary,
+               "weather" : weather,
+               "temp" : temp,
+               "humidity" : humidity,
+               "wind" : wind}
           
+          forecast_info.append(city_forecast)
 
-          if "alerts" in forecast_weather:
-               alertRegex =  re.compile(r'https://www\.\w+\.\w+(\.\w+)*[^\"]')
-               alert = forecast_weather["alerts"][0]["description"]
-               alert = translate(alert)
-               alert = alertRegex.sub(make_link, alert)
+     for i, hour in enumerate(forecast_weather["hourly"]):
+
+          if i >= 12:
+               break
+
+          hour_date = unix_timestamp_converter(hour["dt"], date_format="%H:%M")
+          description = hour["weather"][0]["description"]
+          icon = hour["weather"][0]["icon"]
+          hour_temp = hour["temp"]
+
+          hour_city_forecast = {"hour" : hour_date, "description" : description, "icon" : icon, "temp" : hour_temp }
+          hourly_forecast.append(hour_city_forecast)
+     
+
+     if "alerts" in forecast_weather:
+          alertRegex =  re.compile(r'https://www\.\w+\.\w+(\.\w+)*[^\"]')
+          alert = forecast_weather["alerts"][0]["description"]
+          alert = translate(alert)
+          alert = alertRegex.sub(make_link, alert)
 
      return render(request, "city.html", {
           "city" : city,
