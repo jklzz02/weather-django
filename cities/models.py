ccf31492e8259ed34f0c2fcbd9e0b91e79b63f82
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-10-22 13:22:18 +0200

    create city model and populated database

---
+++ cities/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+    
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    country_code = models.CharField(max_length=2)
+    population = models.IntegerField()
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['country_code']),
+        ]
--- main/urls.py
+++ main/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
-    path('city/', views.city, name="city"),
+    path('', include('cities.urls'))
+
 ]
