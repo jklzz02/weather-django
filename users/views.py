0354f9816f8993261ce10f3542df9e65058545c6
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-10-22 20:21:20 +0200

    users app intiated

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def register(request):
+    return HttpResponse("register")
+
+def login(request):
+    return HttpResponse("login")
+
+def profile(request):
+    return HttpResponse("user profile")
