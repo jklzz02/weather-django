8c785751d91f98e948d43becc313dce84358ce28
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2024-11-07 19:19:51 +0100

    js updated

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,41 +1,39 @@
-// html elements
-const themeToggleBtn = document.querySelector('.theme-toggle');
-const lightIcon = document.getElementById('lightIcon');
-const darkIcon = document.getElementById('darkIcon');
+document.addEventListener("DOMContentLoaded", () => {
 
-// get current theme
-const theme = localStorage.getItem('theme');
+    const themeToggleBtn = document.querySelector('.theme-toggle');
+    const lightIcon = document.getElementById('lightIcon');
+    const darkIcon = document.getElementById('darkIcon');
 
-// adding classes to the body and icons based on current theme
-if (theme) {
-    document.body.classList.add(theme);
-    darkIcon.classList.add('hidden');
-}
-else {
-    lightIcon.classList.add('hidden');
-}
+    const theme = localStorage.getItem('theme');
 
-// toggle between icons and add to local storace current theme mode
-themeToggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode');
-
-    if (document.body.classList.contains('light-mode')) {
-        localStorage.setItem('theme', 'light-mode');
+    if (theme) {
+        document.body.classList.add(theme);
         darkIcon.classList.add('hidden');
-        lightIcon.classList.remove('hidden');
-        lightIcon.classList.add('fade-in');
-
-    } else {
-        localStorage.removeItem('theme');
+    }
+    else {
         lightIcon.classList.add('hidden');
-        darkIcon.classList.remove('hidden');
-        darkIcon.classList.add('fade-in');
     }
 
-    // timeout that removes animation after it's finished
-    setTimeout(() => {
-        lightIcon.classList.remove('fade-in');
-        darkIcon.classList.remove('fade-in');
-    }, 500);
+    themeToggleBtn.addEventListener('click', () => {
+        document.body.classList.toggle('light-mode');
 
+        if (document.body.classList.contains('light-mode')) {
+            localStorage.setItem('theme', 'light-mode');
+            darkIcon.classList.add('hidden');
+            lightIcon.classList.remove('hidden');
+            lightIcon.classList.add('fade-in');
+
+        } else {
+            localStorage.removeItem('theme');
+            lightIcon.classList.add('hidden');
+            darkIcon.classList.remove('hidden');
+            darkIcon.classList.add('fade-in');
+        }
+
+        setTimeout(() => {
+            lightIcon.classList.remove('fade-in');
+            darkIcon.classList.remove('fade-in');
+        }, 500);
+    });
+    
 });
